530db2d2f6af19ecfb4a63a15484471c80ab6e7e
Gabriel Guevara <gabriel0ggg@gmail.com>
2022-03-04 09:12:48 -0600

    Initial Commit

---
+++ Tabla2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tabla2
+//
+//  Created by Gabriel Guevara Gutiérrez on 04/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
